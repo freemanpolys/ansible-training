6e461c0101c0ee0d12baa01c27403aafcee51af5
James Kokou GAGLO <kokou028666@orange-sonatel.com>
2019-02-11 09:59:00 +0000

    Update labs-environments script

--- labs-environments.sh
+++ labs-environments.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 END=$1
 for ((i=1;i<=END;i++)); do
-    sshort="400$i"
+    mgmtssh="300$i"
+    sshport="400$i"
     httport="500$i"
-    name="ansible$i"
+    mysqlport="600$i"
+    mgmt="mgmt-ansible$i"
+    remote="ansible$i"
 
     if [ -z "$2" ]
     then
-        docker run -d --name $name -p $sshort:22 -p $httport:80 rastasheep/ubuntu-sshd 
+        docker run -d --name $mgmt -p $mgmtssh:22 rastasheep/ubuntu-sshd 
+        docker run -d --name $remote -p $sshport:22 -p $httport:80  -p $mysqlport:3306 rastasheep/ubuntu-sshd 
     else
-        docker rm -f $name
+        docker rm -f $mgmt
+        docker rm -f $remote
     fi 
 
 done
